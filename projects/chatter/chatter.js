ac54629f0a5f38515b6d78a55070b0f7e9aade84
JCGithu <jack.c.gracie@gmail.com>
2021-11-27 13:59:46 +0000

    added chatter

---
+++ projects/chatter/chatter.js
@@ -0,0 +1,80 @@
+const urlParams = new URLSearchParams(window.location.search);
+const u = urlParams.get('channel');
+
+const badgeData = document.createElement('script');
+
+/* badgeData.src = 'https://badges.twitch.tv/v1/badges/global/display';
+document.body.appendChild(badgeData); */
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://badges.twitch.tv/v1/badges/global/display', true);
+xhr.withCredentials = true;
+xhr.setRequestHeader('Accept', 'application/json');
+xhr.send();
+
+console.log(xhr);
+
+/* badgeData.addEventListener('onDataLoad', (data)=> {
+    console.log('loaded!');
+    console.log(data);
+}) */
+
+
+const userData = {
+    'lydiapancakes': "#FFC2A9",
+    "hellovonnie": '#bf94ff',
+    "astoldbyangela": "teal",
+    "colloquialowl": "#fe5f55",
+    "kiwi_fruitbird": "#00B160",
+    'arcasian': '#94588c',
+    'theyeteedotcom': '#3f4be4',
+}
+
+const client = new tmi.Client({
+    channels: [u]
+});
+
+client.on("connected", () => {
+    console.log('Reading from Twitch! ✅');
+});
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+client.connect();
+
+function removeTop(chatDiv) {
+    let display = chatDiv.getBoundingClientRect();
+    if (display.y + display.height > window.innerHeight - 10) {
+      document.querySelector('#chatbox').remove();
+      removeTop(chatDiv);
+    }
+}
+
+function postBox(channel, tags, message, self, italics){
+    console.log(tags);
+    if (tags.username === 'colloquialbot') return;
+    let toAdd = document.createElement('div');
+    for (let u in userData){
+        if (u === tags.username) toAdd.style.backgroundColor = userData[u];
+    }
+    let emotes = formatEmotes(message,tags.emotes);
+    let chatName = document.createElement('b');
+    chatName.innerHTML = `${tags.username}: `;
+    let chatText = document.createElement('span');
+    chatText.innerHTML = emotes;
+    toAdd.id = 'chatbox';
+    toAdd.appendChild(chatName);
+    toAdd.appendChild(chatText);
+    document.body.appendChild(toAdd);
+    removeTop(toAdd);
+}
+
+client.on('chat', (channel, tags, message, self) => {
+    if (tags['custom-reward-id']){
+        if (tags['custom-reward-id'] === '62dbe65f-de95-48db-a985-734f5eb761a3') userData[tags.username] = message;
+    }
+    postBox(channel, tags, message, self, false);
+});
+client.on('action', (channel, tags, message, self) => {postBox(channel, tags, message, self, false)});	
---
+++ projects/chatter/formatEmotes.js
@@ -0,0 +1,45 @@
+function htmlEntities(html) {
+  function it() {
+    return html.map(function (n, i, arr) {
+      if (n.length === 1) {
+        return n.replace(/[\u00A0-\u9999<>\&]/gim, function (i) {
+          return '&#' + i.charCodeAt(0) + ';';
+        });
+      }
+      return n;
+    });
+  }
+  var isArray = Array.isArray(html);
+  if (!isArray) html = html.split('');
+  html = it(html);
+  if (!isArray) html = html.join('');
+  return html;
+}
+  
+function formatEmotes(text, emotes) {
+    var splitText = text.split('');
+    for (var i in emotes) {
+        var e = emotes[i];
+        for (var j in e) {
+            var mote = e[j];
+            if (typeof mote == 'string') {
+                mote = mote.split('-');
+                mote = [parseInt(mote[0]), parseInt(mote[1])];
+                var length = mote[1] - mote[0],
+                empty = Array.apply(null, new Array(length + 1)).map(function () {
+                    return '';
+                });
+                splitText = splitText
+                .slice(0, mote[0])
+                .concat(empty)
+                .concat(splitText.slice(mote[1] + 1, splitText.length));
+                splitText.splice(
+                mote[0],
+                1,
+                `<img class="emoticon" src="http://static-cdn.jtvnw.net/emoticons/v2/${i}/default/light/3.0">`
+                );
+            }
+        }
+    }
+    return htmlEntities(splitText).join('');
+}
