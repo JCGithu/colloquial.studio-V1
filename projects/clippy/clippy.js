5581656297b4477687ec5a6ce45371ad0d9af4cc
JCGithu <jack.c.gracie@gmail.com>
2021-12-16 22:35:59 +0000

    clippy update

--- projects/clippy/clippy.js
+++ projects/clippy/clippy.js
@@ -844,7 +844,7 @@ clippy.Balloon.prototype = {
 
 };
 
-clippy.BASE_PATH = './Agents/';
+clippy.BASE_PATH = './projects/clippy/Agents/';
 
 clippy.load = function (name, successCb, failCb) {
     var path = clippy.BASE_PATH + name;
--- projects/clippy/clippyManager.js
+++ projects/clippy/clippyManager.js
@@ -2,14 +2,14 @@ const urlParams = new URLSearchParams(window.location.search);
 const u = urlParams.get('channel');
 let pointID = urlParams.get('pointID');
 
-pointID = pointID.replace(/_/g, '-');
+if (pointID) pointID = pointID.replace(/_/g, '-');
 
 if (u) {
-  loadScript('./runClippy.js').then(()=>{
+  loadScript('./projects/clippy/runClippy.js').then(()=>{
     console.log('');
   })
 } else {
-  loadScript('./runMenu.js').then(()=>{
+  loadScript('./projects/clippy/runMenu.js').then(()=>{
     console.log('run menu')
   })
 };
