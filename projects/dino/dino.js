1476f656cec806cdd2576dd169f1ed1241cbf8c4
JCGithu <jack.c.gracie@gmail.com>
2021-12-17 15:10:56 +0000

    added loading

--- projects/dino/dino.js
+++ projects/dino/dino.js
@@ -8,8 +8,24 @@ client.connect();
 
 let i = 0;
 
+let req = new XMLHttpRequest();
+req.open('GET', './projects/dino/dino.webm', true);
+req.responseType = 'blob';
+
+req.onload = () =>{
+  console.log('loaded!');
+  let screen = document.getElementById('load');
+  document.body.removeChild(screen);
+}
+req.onerror = (err) => {
+  console.log(err)
+}
+
+req.send();
+
 client.on('message', (channel, tags, message) => {
   if (message === 'rawr') {
+    console.log('order recieved');
     let vid = document.createElement('video');
     vid.src = './projects/dino/dino.webm';
     vid.setAttribute('autoplay', true);
