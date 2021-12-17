0c3db41f8929c0eb66d15890577a14da2fe41fa9
JCGithu <jack.c.gracie@gmail.com>
2021-12-17 18:26:06 +0000

    patched dino

--- projects/dino/dino.js
+++ projects/dino/dino.js
@@ -7,36 +7,57 @@ client.on("connected", () => {console.log('Reading from Twitch! ✅')});
 client.connect();
 
 let i = 0;
+let dino = './projects/dino/dino.webm';
+let dinoLoaded;
 
-let req = new XMLHttpRequest();
-req.open('GET', './projects/dino/dino.webm', true);
-req.responseType = 'blob';
+let xhr = new XMLHttpRequest();
+xhr.open('GET', dino, true);
+xhr.responseType = 'arraybuffer';
 
-req.onload = () =>{
-  console.log('loaded!');
-  let screen = document.getElementById('load');
-  document.body.removeChild(screen);
+xhr.onload = (res) => {
+  let blob = new Blob([res.target.response], {type: 'video/webm'});
+  dinoLoaded = URL.createObjectURL(blob);
+  playDino(true);
+  console.log('init loaded');
 }
-req.onerror = (err) => {
-  console.log(err)
+
+xhr.onprogress = (ev) => {
+  if (ev.lengthComputable){
+    console.log(`${ev.loaded} out of ${ev.total}`);
+  }
 }
 
-req.send();
+xhr.send();
 
-client.on('message', (channel, tags, message) => {
-  if (message === 'rawr') {
-    console.log('order recieved');
-    let vid = document.createElement('video');
-    vid.src = './projects/dino/dino.webm';
-    vid.setAttribute('autoplay', true);
-    vid.setAttribute('type', "video/webm");
-    vid.id = i;
+function playDino(start){
+  let vid = document.createElement('video');
+  vid.src = dinoLoaded;
+  vid.autoplay = true;
+  vid.muted = true;
+  vid.setAttribute('type', "video/webm");
+  vid.id = i;
+  vid.addEventListener('canplaythrough', function() {
+    if (start){
+      let screen = document.getElementById('load');
+      document.body.removeChild(screen);
+    }
     document.body.appendChild(vid);
+    vid.play();
     let target = document.getElementById(i);
     i++;
     target.addEventListener('ended', ()=>{
       console.log('video ended');
       document.body.removeChild(target);
     })
+  })
+  vid.addEventListener('error', function (event) {
+    console.log(event);
+  })
+}
+
+client.on('message', (channel, tags, message) => {
+  if (message === 'rawr') {
+    console.log('order recieved');
+    playDino(false);
   }
 });
