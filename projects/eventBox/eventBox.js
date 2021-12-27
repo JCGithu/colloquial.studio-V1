e46ecd14f83de915c9df240459baff38cda10a06
JCGithu <jack.c.gracie@gmail.com>
2021-12-27 18:23:42 +0000

    added eventbox

---
+++ projects/eventBox/eventBox.js
@@ -0,0 +1,59 @@
+let textWrapper = document.querySelector('.title');
+let message = document.getElementById('message');
+let img = document.getElementById('img');
+const urlParams = new URLSearchParams(window.location.search);
+const t = urlParams.get('target');
+const m = urlParams.get('message');
+const i = urlParams.get('gif');
+const c = urlParams.get('colour');
+
+img.src = './eventBox/ewok.gif';
+
+if (t) {
+  textWrapper.innerHTML = t;
+  let space = 1 - (t.length * 0.05);
+  console.log(space);
+  textWrapper.style.letterSpacing = `${space}rem`
+}
+if (c) textWrapper.style.backgroundColor = c;
+if (m) message.innerHTML = m;
+if (i) img.src = `./eventBox/${i}.gif`;
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.title .letter',
+    translateY: [0, -12, 0],
+    duration: 1500,
+    easing: "easeInOutQuad",
+    delay: (el, i) => (55 * i) + 500
+  })
+
+anime.timeline()
+  .add({
+    targets: '#theBox',
+    translateY: "-200%",
+    duration: 10,
+    easing: "easeOutElastic",
+  })
+  .add({
+    targets: '#theBox',
+    translateY: "30%",
+    duration: 2000,
+    delay: 1000,
+    easing: "easeOutElastic",
+  })
+  .add({
+  targets: '#theBox',
+  translateY: "-200%",
+  duration: 1000,
+  delay: 7000,
+  easing: "easeOutExpo",
+})
+
+
+
+
+
